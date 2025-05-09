21965c76d8c49a9b323021158576f1e2a792d8a2
John Doe <sudvital@gmail.com>
2025-05-09 20:13:01 +0530

    Updated backup script to accept folder name as argument

--- backup_folder.py
+++ backup_folder.py
@@ -2,14 +2,17 @@
 
 import shutil
 import os
+import sys
 
-# Folder to backup
-source_folder = "sample_data"  # Folder you want to back up
+# Get folder name from command-line argument
+if len(sys.argv) != 2:
+    print("Usage: python backup_folder.py <folder_to_backup>")
+    sys.exit(1)
 
-# Backup file name
-backup_file = "sample_data_backup.zip"
+source_folder = sys.argv[1]
+backup_file = f"{source_folder}_backup.zip"
 
 # Create a zip file
-shutil.make_archive('sample_data_backup', 'zip', source_folder)
+shutil.make_archive(source_folder + "_backup", 'zip', source_folder)
 
 print(f"Backup of '{source_folder}' created as '{backup_file}'")
